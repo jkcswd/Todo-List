18cc618e61a4766319ab72c3d8b3d55535627fc8
James Spencer <jameskcs1254@gmail.com>
2021-08-23 13:55:10 +0100

    moved class creation to app module

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
-import { Project, Todo } from "./objects";
+class Todo {
+    constructor(title,description,dueDate, priority, notes){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}
+
+class Project {
+    constructor(title,description){
+        this.title = title;
+        this.description = description;
+        this.todoList = [];
+    }
+
+    addTodoObj(todoObj) {
+        this.todoList.push(todoObj)
+    }
+}
 
 const populateProjects = ()=> {
 
