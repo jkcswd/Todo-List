34d5c69c4d0de90801ba5a63e4be5d925531f610
James Spencer <jameskcs1254@gmail.com>
2021-08-23 11:40:45 +0100

    add basic create functions for objects

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import { Project, Todo } from "./objects";
+
+const createTodo = (title,description,dueDate, priority, notes) => {
+    const todo = new Todo(title,description,dueDate, priority, notes);
+}
+const createProject = (title,description) => {
+    const project = new Project(title,description);
+}
+const deleteTodo = (project,todo) => {
+
+}
+const editTodo = () => {
+
+}
