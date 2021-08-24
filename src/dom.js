029c2435bf80c6603c1e5178f11f86a72a83a6cc
James Spencer <jameskcs1254@gmail.com>
2021-08-24 11:03:14 +0100

    Add event listeners to get data for use in creating new projects and todos

--- src/dom.js
+++ src/dom.js
@@ -1,24 +1,43 @@
 import { projects } from "./app"
 
 const loadProjects = () => {
-    const sidebar = document.querySelector('.sidebar');
+    const projectsDiv = document.querySelector('.projects');
 
     projects.forEach(project => {
         //O(n^2) canditate for refactor.
         const projectDiv = document.createElement('div');
 
         projectDiv.innerHTML += `<h3>${project.title}</h3><p>${project.description}</p>`
-        sidebar.appendChild(projectDiv);
+        projectsDiv.appendChild(projectDiv);
 
         project.todoList.forEach(todo  => {
             const todoDiv = document.createElement('div');
 
             todoDiv.innerHTML += todo.title;
             projectDiv.appendChild(todoDiv);
-
-
         })
     });
 }
 
-export { loadProjects }
+const getTodo = () => {
+    const todoBtn = document.querySelector('.todo-btn');
+
+    todoBtn.addEventListener('click', ()=> {
+        const data = prompt('Add todo, format: title, description, dueDate, priority, notes');
+
+        return data;
+    })
+
+}
+const getProject = () => {
+    const projectBtn = document.querySelector('.project-btn');
+
+    projectBtn.addEventListener('click', ()=> {
+        const data = prompt('Add Project, format: title, description');
+
+        return data;
+    })
+
+}
+
+export { loadProjects, getProject, getTodo }
