a768e997104edb2aecd9c8abe5cb777269413d79
James Spencer <jameskcs1254@gmail.com>
2021-08-24 12:56:05 +0100

    fix formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import { initProjects } from "./app";
 import { loadProjects, getProject, getTodo} from "./dom"
 
-
 const runApp = (()=>{
     initProjects();
     loadProjects();
     getTodo();
     getProject();
 })();
-
-
-
