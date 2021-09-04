8a2554b356c169e8fc02ffe78342557c3b9cd0c7
James Spencer <jameskcs1254@gmail.com>
2021-09-04 10:03:05 +0100

    Break up loadProjects function into many small functions
    I broke the loadProjects function into many small functions to allow easier debugging and
    readability of code.

--- src/dom.js
+++ src/dom.js
@@ -1,15 +1,12 @@
 import { projects,addProject, addTodo } from "./app"
 
 const loadProjects = () => {
-    //long function look at splitting
     const projectsDiv = document.querySelector('.projects');
     let count = 0;
 
-    projectsDiv.innerHTML = ''
-    projectsDiv.innerHTML += '<h2>Projects</h2><button class="project-btn">+</button>'
+    clearDOM(projectsDiv);
 
     projects.forEach(project => {
-        //O(n^2) canditate for refactor.
         const projectDiv = document.createElement('div');
 
         projectDiv.setAttribute('data-index', `${count}`);
@@ -17,42 +14,57 @@ const loadProjects = () => {
         projectDiv.innerHTML += `<h3>${project.title}</h3><p>${project.description}</p>`
         projectsDiv.appendChild(projectDiv);
 
-        project.todoList.forEach(todo  => {
-            const todoDiv = document.createElement('div');
-
-            todoDiv.innerHTML += todo.title;
-            projectDiv.appendChild(todoDiv);
-        });
-        const todoBtn = document.createElement('button');
-
-        todoBtn.classList.add('todo-btn');
-        todoBtn.innerHTML = '+';
-        projectDiv.appendChild(todoBtn);
+        loadTodoItems(project, projectDiv);
+        addTodoButton(projectDiv);
     });
 
     getProject();
     getTodo();
 }
 
-const getTodo = () => {
-    const todoBtns = document.querySelectorAll('.todo-btn');
-    const saveBtn = document.querySelector('.save-todo');
-    const closeBtn = document.querySelector('.close-todo');
-    const inputs = document.querySelectorAll('.inputs');
-    const modal = document.querySelector('.modal-todo');
-    let project;
+const clearDOM = (projectsDiv) => {
+    projectsDiv.innerHTML = ''
+    projectsDiv.innerHTML += '<h2>Projects</h2><button class="project-btn">+</button>'
+}
 
-    [...todoBtns].forEach( todoBtn => {
+const loadTodoItems = (project, projectDiv) => {
+    project.todoList.forEach(todo  => {
+        const todoDiv = document.createElement('div');
+
+        todoDiv.innerHTML += todo.title;
+        projectDiv.appendChild(todoDiv);
+    });
+}
+
+const addTodoButton = (projectDiv) => {
+    const todoBtn = document.createElement('button');
+
+    todoBtn.classList.add('todo-btn');
+    todoBtn.innerHTML = '+';
+    projectDiv.appendChild(todoBtn);
+}
+
+
+
+const getTodo = () => {
+    const domElementsObject = {
+        todoBtns : document.querySelectorAll('.todo-btn'),
+        saveBtn : document.querySelector('.save-todo'),
+        closeBtn : document.querySelector('.close-todo'),
+        inputs : document.querySelectorAll('.inputs-todo'),
+        modal : document.querySelector('.modal-todo'),
+        project : null
+    }
+    
+    todoBtns.forEach( todoBtn => { 
         todoBtn.addEventListener('click', (e)=> {
             modal.style.display = "block";
             project = parseInt(e.currentTarget.parentElement.dataset.index);
         });
-
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = "none";
-        inputs.forEach( input  => input.value = '');
     });
-
+    
+    closeButton(domElementsObject);
+    
     saveBtn.addEventListener('click', () => {
         modal.style.display = "none";
         const title = document.querySelector('.title-todo').value;
@@ -60,24 +72,24 @@ const getTodo = () => {
         const dueDate = document.querySelector('.due-date-todo').value;
         const priority = document.querySelector('.priority-todo').value;
         const notes = document.querySelector('.notes-todo').value;
-            
+
+        inputs.forEach(input  => input.value = '');
         addTodo(title,description,dueDate,priority,notes,project);
         loadProjects();
-        inputs.forEach(input  => input.value = '');
     });            
-    })
 }
 
-const getProject = () => {
+const getProject = () => { //gets called multiple times???
     const projectBtn = document.querySelector('.project-btn');
     const modal = document.querySelector('.modal-project');
     const saveBtn = document.querySelector('.save-project');
     const closeBtn = document.querySelector('.close-project');
-    const inputs = document.querySelectorAll('.inputs');
+    const inputs = document.querySelectorAll('.inputs-project');
 
     projectBtn.addEventListener('click', ()=> {
         modal.style.display = "block";
     });
+
     closeBtn.addEventListener('click', () => {
         modal.style.display = "none";
         inputs.forEach( input  => input.value = '');
@@ -88,11 +100,17 @@ const getProject = () => {
         const title = document.querySelector('.title-project').value;
         const description = document.querySelector('.description-project').value;
 
+        inputs.forEach(input  => input.value = '');
         addProject(title,description);
         loadProjects();
-        inputs.forEach(input  => input.value = '');
     });
-
 }
 
-export { loadProjects, getProject, getTodo }
+const closeButton = (domElementsObject) => {
+    domElementsObject.closeBtn.addEventListener('click', () => {
+        domElementsObject.modal.style.display = "none";
+        domElementsObject.inputs.forEach( input  => input.value = '');
+    });
+}
+
+export { loadProjects }
