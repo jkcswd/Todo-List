6be5c2150efc7d692e7b1f9e442102be4c1c7d03
James Spencer <jameskcs1254@gmail.com>
2021-10-14 19:42:21 +0100

    corrected linter sugestions

--- src/app.js
+++ src/app.js
@@ -9,48 +9,49 @@ class Todo {
 }
 
 class Project {
-    constructor(title,description){
-        this.title = title;
-        this.description = description;
-        this.todoList = [];
-    }
+  constructor (title, description) {
+    this.title = title
+    this.description = description
+    this.todoList = []
+  }
 
-    set setTitle(title){
-        this.title = title;
-    }
+  set setTitle(title){
+      this.title = title;
+  }
 
-    set setDesciption(desc){
-        this.description = desc;
-    }
+  set setDesciption(desc){
+      this.description = desc;
+  }
 
-    addTodoObj(todoObj) {
-        this.todoList.push(todoObj)
-    }
+  addTodoObj (todoObj) {
+    this.todoList.push(todoObj)
+  }
 }
 
-let projects = []; //data holder 
+const projects = [] // data holder
 
-const addProject = (title,description) => {
-    const newProject = new Project(title,description);
-    projects.push(newProject);
+const addProject = (title, description) => {
+  const newProject = new Project(title, description)
+  projects.push(newProject)
 }
 
 const addTodo = (formFieldsObject) => {
-    const newTodo = new Todo(formFieldsObject.title,
-                             formFieldsObject.description,
-                             formFieldsObject.dueDate, 
-                             formFieldsObject.priority, 
-                             formFieldsObject.notes);
+  const newTodo = new Todo(
+    formFieldsObject.title,
+    formFieldsObject.description,
+    formFieldsObject.dueDate,
+    formFieldsObject.priority,
+    formFieldsObject.notes
+  )
 
-    projects[formFieldsObject.project].todoList.push(newTodo);
+  projects[formFieldsObject.project].todoList.push(newTodo)
 }
 
 const initProjects = () => {
-    //Should this be here or in index.js?
-    const defaultProject = new Project('Default', 'Default List');
-    const exampleTodo = new Todo('Todo', 'example', '1/1/1','1','This is an example todo item', defaultProject);
-    defaultProject.addTodoObj(exampleTodo);
-    projects.push(defaultProject);
+  const defaultProject = new Project('Default', 'Default List')
+  const exampleTodo = new Todo('Todo', 'example', '1/1/1', '1', 'This is an example todo item', defaultProject)
+  defaultProject.addTodoObj(exampleTodo)
+  projects.push(defaultProject)
 }
 
-export { projects, initProjects, addProject, addTodo };
+export { projects, initProjects, addProject, addTodo }
